b53a59c89bf9a3d139631095c0e42a790dce5211
Ergys Hila <ergysmania@gmail.com>
2023-12-12 16:56:27 -0500

    Add user store for authentication

--- my-app/src/lib/firebase.ts
+++ my-app/src/lib/firebase.ts
@@ -2,6 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
+import type { User } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsDOujb18BsEuBKOSSC3vCxZWy4PMKkTQ",
@@ -18,3 +22,29 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
